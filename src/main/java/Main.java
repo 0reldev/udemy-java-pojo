76056573ff8ca0cc57ab0b2e4e68f5df33d33289
Aurélien Lonni <alonni@norsys.fr>
2023-03-27 18:04:04 +0200

    Lesson about POJO

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            Student s = new Student("S92300" + i,
+                    switch (i) {
+                        case 1 -> "Mary";
+                        case 2 -> "Carol";
+                        case 3 -> "Tim";
+                        case 4 -> "Lisa";
+                        default -> "Anonymous";
+                    },
+                    "05/11/1985",
+                    "Java Masterclass");
+            System.out.println(s);
+        }
+    }
+}
