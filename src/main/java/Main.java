b8e7beec4e401c8674fd79d7afda42c4a0b03ea8
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-03-30 17:40:55 +0200

    Lessons about record objects.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 5; i++) {
-            Student s = new Student("S92300" + i,
+            LPAStudent s = new LPAStudent("S92300" + i,
                     switch (i) {
                         case 1 -> "Mary";
                         case 2 -> "Carol";
@@ -15,5 +15,17 @@ public class Main {
                     "Java Masterclass");
             System.out.println(s);
         }
+
+        Student pojoStudent = new Student("S4898", "Leia", "05/04/1978", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("S4899", "Luke", "05/04/1978", "Java Masterclass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+     //   recordStudent.setClassList(recordStudent.classList()) + ", Java OCP Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
     }
 }
